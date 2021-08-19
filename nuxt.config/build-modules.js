ab20a0bd3a54bee11fe574ba9c8b6d8cfe36feda
John Ian J. Buñag <jianbunag@gmail.com>
2021-08-19 03:51:12 +0000

    PF-1: Scaffold Static Site Generate Framework

---
+++ nuxt.config/build-modules.js
@@ -0,0 +1,7 @@
+// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+export const buildModules = [
+  '@nuxt/typescript-build', // https://go.nuxtjs.dev/typescript
+  '@nuxtjs/composition-api/module',
+  '@nuxtjs/stylelint-module', // https://go.nuxtjs.dev/stylelint
+  '@nuxtjs/vuetify', // https://go.nuxtjs.dev/vuetify
+]
