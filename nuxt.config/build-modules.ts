14c7194ab0ded91f48b50d905a80a26106af16a1
John Ian J. Buñag <jianbunag@gmail.com>
2021-09-07 03:23:30 +0000

    PF-9: Set up SEO

--- nuxt.config/build-modules.ts
+++ nuxt.config/build-modules.ts
@@ -1,3 +1,23 @@
+function normalizeHost (host: string): string {
+  const trimmedHost = host.trim()
+
+  if (trimmedHost === 'localhost') { return 'http://127.0.0.1' }
+
+  return trimmedHost
+}
+
+function normalizeName (name: string): string {
+  return name.replace(/[/:]+/g, '-')
+}
+
+function createSitemap (host: string, name?: string) {
+  return {
+    hostname: host,
+    trailingSlash: true,
+    ...(name ? { path: `${name}.sitemap.xml` } : {}),
+  }
+}
+
 // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 export const buildModules = [
   '@nuxt/typescript-build', // https://go.nuxtjs.dev/typescript
@@ -6,32 +26,33 @@ export const buildModules = [
   '@nuxtjs/stylelint-module', // https://go.nuxtjs.dev/stylelint
   '@nuxtjs/vuetify', // https://go.nuxtjs.dev/vuetify
   ...(() => {
-    const {
-      HOST: host = '',
-      PORT: port,
-      BASE: base = '/',
-    } = process.env
-    const normalizedHost = host.replace('localhost', 'http://127.0.0.1')
+    const { HOST: host = '', PORT: port } = process.env
     const hostSuffix = port ? `:${port}` : ''
-    const hostname = `${normalizedHost}${hostSuffix}`
-    const baseHostname = `${hostname}${base}`
+    const hostname = `${normalizeHost(host)}${hostSuffix}`
+    const robots = (() => {
+      const base = process.env.BASE || '/'
+      const baseHostname = `${hostname}${base}`
+
+      return {
+        Allow: '/',
+        Host: baseHostname,
+        Sitemap: `${baseHostname}sitemap.xml`,
+      }
+    })()
+    const sitemaps = (() => {
+      const extraHosts = (process.env.EXTRA_HOSTS || '')
+        .split(',')
+        .filter(Boolean)
+        .map(normalizeHost)
+        .map(extraHost => [extraHost, normalizeName(extraHost)])
+        .map(([extraHost, name]) => createSitemap(extraHost, name))
+
+      return [createSitemap(hostname), ...extraHosts]
+    })()
 
     return [
-      [
-        '@nuxtjs/robots',
-        {
-          Allow: '/',
-          Host: baseHostname,
-          Sitemap: `${baseHostname}sitemap.xml`,
-        },
-      ],
-      [
-        '@nuxtjs/sitemap',
-        {
-          hostname,
-          trailingSlash: true,
-        },
-      ],
+      ['@nuxtjs/robots', robots],
+      ['@nuxtjs/sitemap', sitemaps],
     ]
   })(),
 ]
