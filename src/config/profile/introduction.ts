55d48bbaeece06d25cbca8f3351254e8ad705d3b
Ian <jianbunag@gmail.com>
2021-11-14 11:01:26 +0800

    Update title to software developer

--- src/config/profile/introduction.ts
+++ src/config/profile/introduction.ts
@@ -2,6 +2,8 @@
  * Separated to own module so that it may be imported from nuxt config without
  *  module aliasing
  */
-export const introduction = 'I am a Full Stack Engineer - with a strong belief in behavior and test driven development approach, importance of code readability and workflow optimization through development tooling.'
+const title = 'Software Developer'
 
-export const shortIntroduction = 'I am Full Stack Engineer with a strong belief in behavior and test driven development, importance of code readability and workflow optimization through tooling.'
+export const introduction = `I am a ${title} - with a strong belief in behavior and test driven development approach, importance of code readability and workflow optimization through development tooling.`
+
+export const shortIntroduction = `I am a ${title} with a strong belief in behavior and test driven development, importance of code readability and workflow optimization through tooling.`
