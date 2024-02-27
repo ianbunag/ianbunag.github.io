ebb2d91f0d8859f10c47103b9c8a4b04d55a820d
Ian Buñag <jianbunag@gmail.com>
2024-02-27 19:53:24 +0800

    Update introduction

--- src/config/profile/introduction.ts
+++ src/config/profile/introduction.ts
@@ -5,13 +5,15 @@ import moment from 'moment'
  *  module aliasing
  */
 
-const constant = 'Experienced Software Developer with hands-on expertise in crafting clean code, optimizing workflows through developer tools, and adopting a behavior and test driven development approach to consistently deliver high-quality software'
+const constant =
+  'Experienced Software Developer with hands-on expertise in crafting clean code, optimizing workflows through developer tools, and adopting a behavior and test-driven development approach to consistently deliver high-quality software. Proven track record through key contributions including a 2 month delivery of a full platform migration software, and test parallelization saving $10,000+'
 
 function dynamic (): string {
-  const yearsOfExperience = moment(moment()).diff(new Date('2019 June'), 'years', false)
+  const yearsOfExperience = moment(moment())
+    .diff(new Date('2019 June'), 'years', false)
     .toFixed(0)
 
-  return `Experienced Software Developer with over ${yearsOfExperience} years of hands-on expertise in crafting clean code, optimizing workflows through developer tools, and adopting a behavior and test driven development approach to consistently deliver high-quality software`
+  return `Experienced Software Developer with over ${yearsOfExperience} years of hands-on expertise in crafting clean code, optimizing workflows through developer tools, and adopting a behavior and test-driven development approach to consistently deliver high-quality software. Proven track record through key contributions including a 2 month delivery of a full platform migration software, and test parallelization saving $10,000+`
 }
 
 export const introduction = {
