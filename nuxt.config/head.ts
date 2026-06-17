e7c25d4f0436f462139be293db8507afd31accb1
John Ian Bunag <hey@ianbunag.dev>
2026-06-16 23:14:47 -0700

    Allow loading of data: images

--- nuxt.config/head.ts
+++ nuxt.config/head.ts
@@ -38,6 +38,7 @@ export const head = {
         'script-src \'self\' \'unsafe-inline\' \'unsafe-eval\';',
         'style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com;',
         'font-src \'self\' https://fonts.gstatic.com;',
+        'img-src \'self\' data:;',
       ].join(' '),
     },
   ],
